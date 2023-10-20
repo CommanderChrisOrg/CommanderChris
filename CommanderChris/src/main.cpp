daeba7ec27dc41f15033657758a21260ff04daf2
Bohdan Hrotovytskyy <hrotovb001@users.noreply.github.com>
2023-10-19 23:48:55 -0400

    Big progress <3

--- CommanderChris/src/main.cpp
+++ CommanderChris/src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <sstream>  
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
+#include "ftxui/component/component_options.hpp"
+#include "ftxui/screen/color.hpp"
+#include "scroller.hpp"
 
 using namespace ftxui;
 
@@ -27,20 +32,24 @@ std::string exec(std::string command) {
    return result;
 }
 
-Element TermUI(Component &leftContainer) {
+Element TermUI(std::vector<Element> term_lines, Component &input_box) {
     // Return a horizontal box containing two vertical boxes separated by a separator.
     return hbox({
         // Left vertical box containing the terminal input.
         vbox({
             text(L"Terminal"),
-            leftContainer->Render() | border,
+            vbox({
+                vbox(std::move(term_lines)) | focusPositionRelative(0, 1) | yframe | flex,
+                separator(),
+                input_box->Render(),
+            }) | border,
         }) | flex,
         separator(),
         // Right vertical box with application details.
         vbox({
             text(L"CommanderChris") | bold,
             text(L"Details and content can be added here."),
-        }) | flex,
+        }),
     });
 }
 
@@ -48,26 +57,26 @@ void run_term() {
     // Set the placeholder text for the input field.
     std::string placeholder = "Type commands here...";
     std::string input_content;
+    std::vector<Element> term_lines;
 
     // Create an input component where users can type commands.
-    Component input_field = Input({.content = &input_content, .placeholder = &placeholder});
-
-    // Create a vertical container and add the input component to it.
-    Component leftContainer = Container::Vertical({
-        input_field,
-    });
+    InputOption opt = InputOption::Default();
+    opt.content = &input_content;
+    opt.placeholder = &placeholder;
+    opt.on_enter = [&]{
+        term_lines.emplace_back(color(Color::Grey0, text(input_content)));
+        std::string out = exec(input_content);
+        std::stringstream out_stream(out);
+        std::string line;
+        while(getline(out_stream, line, '\n')) term_lines.emplace_back(text(line));
+        input_content = "";
+    };
+    Component input_box = Input(opt);
     
-    auto screen = ScreenInteractive::TerminalOutput();
-    auto renderer = Renderer(leftContainer, [&] { return TermUI(leftContainer); });
-    auto component = CatchEvent(renderer, [&](Event event) {
-      if (event == Event::Special({10})) {
-        std::cout << exec(input_content);
-        return true;
-      }
-      return false;
-    });
+    auto screen = ScreenInteractive::Fullscreen();
+    auto renderer = Renderer(input_box, [&] { return TermUI(term_lines, input_box); });
     // Enter the main loop for the screen with the layout renderer.
-    screen.Loop(component);
+    screen.Loop(renderer);
 }
 
 // Main function to run the program.
