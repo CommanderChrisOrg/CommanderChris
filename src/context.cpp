7219b1b3ce583a418d2b1d0b02338069fde18e5f
Bohdan Hrotovytskyy <hrotovb001@users.noreply.github.com>
2023-11-15 18:08:35 -0500

    Fixed Paths with Spaces Not Escapes

--- src/context.cpp
+++ src/context.cpp
@@ -4,13 +4,25 @@
 
 using namespace boost::filesystem;
 
+std::string get_path_string(path p){
+    std::string ret;
+
+    for (auto it = p.begin(); it != p.end(); ++it) {
+        if (it->string().find(' ') != std::string::npos) ret += "'" + it->string() + "'";
+        else ret += it->string();
+        if (it != p.begin()) ret += "/";
+    }
+
+    return ret;
+}
+
 std::string get_dir_files(path p){
     std::string ret = "";
     if(is_directory(p)) {
         int count = 0;
         auto it = directory_iterator(p);
         auto end = directory_iterator();
-        for (;it != end && count < 15; ++it, ++count) ret += it->path().filename().string() + "\n";
+        for (;it != end && count < 15; ++it, ++count) ret += get_path_string(it->path()) + "\n";
     }
     return ret;
 }
@@ -20,13 +32,13 @@ std::string get_file_context(){
     std::string ret = "";
 
     p = current_path().parent_path().parent_path();
-    ret += "files and directories in " + p.string() + ":\n" + get_dir_files(p) + "\n\n";
+    ret += "files and directories in " + get_path_string(p) + ":\n" + get_dir_files(p) + "\n\n";
 
     p = current_path().parent_path();
-    ret += "files and directories in " + p.string() + ":\n" + get_dir_files(p) + "\n\n";
+    ret += "files and directories in " + get_path_string(p) + ":\n" + get_dir_files(p) + "\n\n";
 
      p = current_path();
-    ret += "files and directories in current directory " + p.string() + ":\n" + get_dir_files(p) + "\n\n"; 
+    ret += "files and directories in current directory (" + get_path_string(p) + "):\n" + get_dir_files(p) + "\n\n"; 
 
     return ret;
 }
