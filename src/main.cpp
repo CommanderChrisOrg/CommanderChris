29606fc934488a2efcb5c8b8f98181a937128c46
Bohdan Hrotovytskyy <hrotovb001@users.noreply.github.com>
2023-11-15 00:49:08 -0500

    Software like Vim are now supported. Turns out there is a system() call that does just what we need.

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,7 @@
 #include <ftxui/component/screen_interactive.hpp>
 #include "gpt.hpp"
 
-
 using namespace ftxui;
-
-std::string exec(std::string command) {
-   char buffer[128];
-   std::string result = "";
-
-   // Open pipe to file
-   FILE* pipe = popen(command.c_str(), "r");
-   if (!pipe) {
-      return "popen failed!";
-   }
-
-   // read till end of process:
-   while (!feof(pipe)) {
-
-      // use buffer to read and add to result
-      if (fgets(buffer, 128, pipe) != NULL)
-         result += buffer;
-   }
-
-   pclose(pipe);
-   return result;
-}
 
 bool run_confirmation_ui() {
     auto screen = ScreenInteractive::TerminalOutput();
@@ -65,7 +42,7 @@ int main(int argc, char **argv) {
 
     display_command(command);
 
-    if(command != "" && run_confirmation_ui()) std::cout << std::endl << exec(command) << std::endl;
+    if(command != "" && run_confirmation_ui()){ std::cout << std::endl; system(command.c_str()); std::cout << std::endl; }
 
     return 0;
 }
