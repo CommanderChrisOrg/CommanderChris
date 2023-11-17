24e2091e191bcd3126d3575e8db247c36ba6af94
Bohdan Hrotovytskyy <hrotovb001@users.noreply.github.com>
2023-11-17 14:00:42 -0500

    Should No Longer Have to Restart Terminal After Setup Script

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 curl -o ~/.chris_bash https://raw.githubusercontent.com/CommanderChrisOrg/CommanderChris/main/.chris_bash
 echo "source ~/.chris_bash" >> ~/.bashrc
 echo "source ~/.chris_bash" >> ~/.zshrc
+source ~/.chris_bash
