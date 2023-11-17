8f19c849e16300ec612a92fd283f98bd9b6400ca
Bohdan Hrotovytskyy <hrotovb001@users.noreply.github.com>
2023-11-17 18:39:08 -0500

    setup.sh updated to seperate zsh and bash setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 curl -o ~/.chris_bash https://raw.githubusercontent.com/CommanderChrisOrg/CommanderChris/main/.chris_bash
+curl -o ~/.chris_zsh https://raw.githubusercontent.com/CommanderChrisOrg/CommanderChris/main/.chris_zsh
 
 if ! grep -q "source ~/.chris_bash" ~/.bashrc; then
     echo -e "\nsource ~/.chris_bash" >> ~/.bashrc
 fi
 
-if ! grep -q "source ~/.chris_bash" ~/.zshrc; then
+if ! grep -q "source ~/.chris_zsh" ~/.zshrc; then
     echo -e "\nsource ~/.chris_zsh" >> ~/.zshrc
 fi
+
+exec $SHELL
