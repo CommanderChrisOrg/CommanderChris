63ca9bdce8026024f14166903a353cfcacb12e1c
Bohdan Hrotovytskyy <hrotovb001@users.noreply.github.com>
2023-11-23 18:35:44 -0500

    removed scrapers as they are not used in the code

--- src/scrapers/get_software_macos.sh
+++
@@ -1,7 +0,0 @@
-# Output file
-output_file="installed_programs_macos.txt"
-
-# Use system_profiler to get a list of installed applications and write to file
-system_profiler SPApplicationsDataType > "$output_file"
-
-echo "The list of installed applications has been saved to $output_file"
