31083ad9252a71cafd54f7d13bd6a34f8058b62e
Bohdan Hrotovytskyy <hrotovb001@users.noreply.github.com>
2023-11-26 18:31:11 -0500

    Updated CMake and changed name of chris executable

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -o /usr/local/bin/chris  https://github.com/CommanderChrisOrg/CommanderChris/releases/download/v0.0.0/chris-macos
-chmod +x /usr/local/bin/chris
+curl -o /usr/local/bin/chris_exe  https://github.com/CommanderChrisOrg/CommanderChris/releases/download/v0.0.0/chris-macos
+chmod +x /usr/local/bin/chris_exe
 curl -sSL https://raw.githubusercontent.com/CommanderChrisOrg/CommanderChris/main/setup.sh | bash
