fb213ea84c9d8b8c3cd9538e93cb7ab49fb2d410
Bohdan Hrotovytskyy <hrotovb001@users.noreply.github.com>
2023-11-26 22:39:43 -0500

    Added sudo to part of install script that goes to local bin

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -o /usr/local/bin/chris_exec -L https://github.com/CommanderChrisOrg/CommanderChris/releases/download/v0.0/chris-macos
-chmod +x /usr/local/bin/chris_exec
+sudo curl -o /usr/local/bin/chris_exec -L https://github.com/CommanderChrisOrg/CommanderChris/releases/download/v0.0/chris-macos
+sudo chmod +x /usr/local/bin/chris_exec
 curl -sSL https://raw.githubusercontent.com/CommanderChrisOrg/CommanderChris/main/setup.sh | bash
