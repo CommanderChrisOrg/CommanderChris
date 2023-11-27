cf7696f00e650e65d58d1060880fd9464219d89c
Bohdan Hrotovytskyy <hrotovb001@users.noreply.github.com>
2023-11-26 21:41:27 -0500

    Fixed the Install Script (Installs with redirect)

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -o ~/.chris_bash https://raw.githubusercontent.com/CommanderChrisOrg/CommanderChris/main/.chris_bash
-curl -o ~/.chris_zsh https://raw.githubusercontent.com/CommanderChrisOrg/CommanderChris/main/.chris_zsh
+curl -o ~/.chris_bash -L https://raw.githubusercontent.com/CommanderChrisOrg/CommanderChris/main/.chris_bash
+curl -o ~/.chris_zsh -L https://raw.githubusercontent.com/CommanderChrisOrg/CommanderChris/main/.chris_zsh
 
 if ! grep -q "source ~/.chris_bash" ~/.bashrc; then
     echo -e "\nsource ~/.chris_bash" >> ~/.bashrc
